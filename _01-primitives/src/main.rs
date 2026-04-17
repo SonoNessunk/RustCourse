8a612c2dd7ef2c080c69fcfb46d3a9e26e60106a
SonoNessunk <91794916+SonoNessunk@users.noreply.github.com>
2026-04-17 20:28:38 +0200

    .

---
+++ _01-primitives/src/main.rs
@@ -0,0 +1,51 @@
+// Primitives
+
+fn main() {
+    // Integer
+    // Signed i8 .. i128
+    // Unsigned u8 .. u128
+
+    println!("\n---------------------------------\n");
+
+    println!("Integer\n");
+    let x: i32 = -42;
+    let y: u64 = 100;
+
+    println!("Signed Integer: {}", x);
+    println!("Unsigned Integer: {}", y);
+
+    println!("\n---------------------------------\n");
+
+    // Float
+    // f32, f64
+
+    println!("Float\n");
+
+    let x: f32 = 3.1415;
+
+    println!("Float: {}", x);
+
+    println!("\n---------------------------------\n");
+
+    // Boolean
+    // bool
+
+    println!("Boolean\n");
+
+    let x: bool = true;
+
+    println!("Bool: {}", x);
+
+    println!("\n---------------------------------\n");
+
+    // Character
+    // char
+
+    println!("Character\n");
+
+    let x: char = 'a';
+
+    println!("Char: {}", x);
+
+    println!("\n---------------------------------\n");
+}
